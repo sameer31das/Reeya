756217c9b7394c0b9edc90274c2daefd433b13e8
sameer <sameer31.das@gmail.com>
2021-03-26 17:23:52 +0530

    track pages

--- src/app/track-shipment-detail.ts
+++ src/app/track-shipment-detail.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ShareServices } from './app.services';
+import { DialogPopupComponent } from "./dialogpopup/dialogpopup.component";
 
 @Component({
   selector: "app-track-shipment-detail",
@@ -8,9 +10,21 @@ import { ShareServices } from './app.services';
 })
 export class TrackShipmentDetailComponent implements OnInit {
   
-  constructor(private _sharedService: ShareServices) { }
+  constructor(private _sharedService: ShareServices,public dialog: MatDialog) { }
   ngOnInit() {
  
 
+  }
+  openDialog(id: number) {
+    const dialogRef = this.dialog.open(DialogPopupComponent, {
+      width: "50%",
+      height: "70%",
+      data: { id: id },
+      disableClose:true,
+      hasBackdrop:true
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 }
